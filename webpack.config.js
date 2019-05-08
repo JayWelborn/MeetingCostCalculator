4612bf1dcde4688d45308ef00efa7a962aa3951a
jaywelborn <jesse.welborn@gmail.com>
2019-05-07 21:57:54 -0400

    Modified Folder Structure for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
+      template: path.join(__dirname, 'docs', 'index.html')
     })
   ]
 }
