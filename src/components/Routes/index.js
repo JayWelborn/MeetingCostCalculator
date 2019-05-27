8cae38dfca4ea74bb61acc134011ae7b66a7f7b1
jaywelborn <jesse.welborn@gmail.com>
2019-05-26 21:35:21 -0400

    Fixed a lazy loading problem

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -12,12 +12,14 @@ export default class Routes extends Component {
   render() {
     return (
       <main className="container">
-        <Switch>
-          <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+
             <Route path="/calculator" component={Calculator} />
             <Route path="/" component={Home} />
-          </Suspense>
-        </Switch>
+
+          </Switch>
+        </Suspense>
       </main>
     );
   }
